209de76bd1ef3f36dab37352435d001933855a97
Sathvik C <sathvikchekkali123@gmail.com>
2024-02-02 23:39:37 +0530

    shybash app

---
+++ src/main/java/se_lab/shybash_app/App.java
@@ -0,0 +1,13 @@
+package se_lab.shybash_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
